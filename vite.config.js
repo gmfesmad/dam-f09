7f622877d5451a454e078b90251276a4647af1a2
gmfesmad <gmf@esmad.ipp.pt>
2025-05-21 19:07:56 +0100

    Tic Tac Toe

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte(),
+    VitePWA({
+      manifest: {
+        name: "Tic Tac Toe",
+        short_name: "Tic Tac Toe",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
